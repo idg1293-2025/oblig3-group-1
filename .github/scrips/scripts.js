d867baf64c8c9aba84994a3cc5fc318d8022186e
BeevieKyu <marie.a.klausen@gmail.com>
2025-04-28 12:39:14 +0200

    javascriptadded

--- .github/scrips/scripts.js
+++ .github/scrips/scripts.js
@@ -16,11 +16,15 @@ gsap.utils.toArray(".section").forEach(section => {
       trigger: section,
       start: "top 80%", // Start the animation when the top of the section reaches 80% down the viewport
       //end: "top 30%", //(Optional?) end point — only really used if you want animation tied to scrolling
-      toggleActions: "play none none none", 
+      toggleActions: "play none none none",
       // What to do when entering/leaving the trigger area:
       // play on enter, do nothing on leave/back
 
       markers: true, //shows marker on where start and end is. Removes later
     }
   });
+});
+
+document.getElementById('evil').addEventListener('click', function () {
+  this.classList.add('walk'); // Adds the animation class
 });
